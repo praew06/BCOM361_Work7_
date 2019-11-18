99365b44fb165b5f3003c75cb5f30a6687da5148
praew06 <42631169+praew06@users.noreply.github.com>
2019-11-18 10:52:32 +0700

    Add files via upload

---
+++ j0902.java
@@ -0,0 +1,42 @@
+import java.io.*;
+public class j0902 {
+  public static void main (String args[]) throws IOException {
+    int i = 0,t1,t2;
+    String b,categoryid;
+    String[] fields;
+    String[] recs1 = new String[30];
+    String[] recs2 = new String[30];
+    FileReader fin = new FileReader("data_products.txt");
+    BufferedReader bin = new BufferedReader (fin);
+    while ((b =  bin.readLine()) != null) {
+      recs1[i] = b;
+      i = i + 1;
+    }
+    fin.close();
+    t1 = i;
+    i = 0;
+
+    FileReader fin2 = new FileReader("data_cat.txt");
+    BufferedReader bin2 = new BufferedReader (fin2);
+    while ((b =  bin2.readLine()) != null) {
+      recs2[i] = b;
+
+      i = i + 1;
+    }
+    fin2.close();
+    t2 = i;
+
+    for(int j=0;j<t1;j++) {
+      fields = recs1[j].split(",");
+      System.out.println("Show Products"+" = "+fields[0] +" , "+ fields[1] +" , "+  fields[2]+" , "+  fields[3]+" , "+  fields[4]);
+     categoryid = fields[0];
+      for(int p=0;p<t2;p++) {
+        fields = recs2[p].split(",");
+        if (fields[0].equals(categoryid)) {
+          System.out.println("Show Cate"+" = "+fields[1]);
+
+        }
+      }
+    }
+  }
+}
